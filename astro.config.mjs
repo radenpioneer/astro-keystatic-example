457277852cc32ba9c1fdd5e0ad843ec9a4bab438
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2025-03-19 14:04:07 +0000

    add site content configuration and update project dependencies; remove unused about page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-
 import cloudflare from '@astrojs/cloudflare'
+import react from '@astrojs/react'
+import keystatic from '@keystatic/astro'
 
 // https://astro.build/config
 export default defineConfig({
   redirects: {
     '/sngrcreative': 'https://sngrcreative.pages.dev'
   },
-
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     }
-  })
+  }),
+  integrations: [react(), keystatic()]
 })
